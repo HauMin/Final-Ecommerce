71db1aaa5c5b4635676c56d572b582839ce7d5d7
DucVinh2609 <ducvinhnguyen2609@gmail.com>
2019-06-15 14:57:54 +0700

    hot products

--- db/schema.rb
+++ db/schema.rb
@@ -21,50 +21,48 @@ ActiveRecord::Schema.define(version: 2019_06_12_032431) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
-    t.bigint "users_id"
-    t.bigint "products_id"
-    t.bigint "person_post_news_id"
+    t.bigint "user_id"
+    t.bigint "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["person_post_news_id"], name: "index_comments_on_person_post_news_id"
-    t.index ["products_id"], name: "index_comments_on_products_id"
-    t.index ["users_id"], name: "index_comments_on_users_id"
+    t.index ["product_id"], name: "index_comments_on_product_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "contacts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "users_id"
+    t.bigint "user_id"
     t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["users_id"], name: "index_contacts_on_users_id"
+    t.index ["user_id"], name: "index_contacts_on_user_id"
   end
 
   create_table "histories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "users_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["users_id"], name: "index_histories_on_users_id"
+    t.index ["user_id"], name: "index_histories_on_user_id"
   end
 
   create_table "image_products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "image"
-    t.bigint "products_id"
+    t.bigint "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["products_id"], name: "index_image_products_on_products_id"
+    t.index ["product_id"], name: "index_image_products_on_product_id"
   end
 
   create_table "order_details", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "quanitily"
     t.float "price"
-    t.bigint "orders_id"
-    t.bigint "products_id"
-    t.bigint "histories_id"
+    t.bigint "order_id"
+    t.bigint "product_id"
+    t.bigint "history_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["histories_id"], name: "index_order_details_on_histories_id"
-    t.index ["orders_id"], name: "index_order_details_on_orders_id"
-    t.index ["products_id"], name: "index_order_details_on_products_id"
+    t.index ["history_id"], name: "index_order_details_on_history_id"
+    t.index ["order_id"], name: "index_order_details_on_order_id"
+    t.index ["product_id"], name: "index_order_details_on_product_id"
   end
 
   create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -72,19 +70,10 @@ ActiveRecord::Schema.define(version: 2019_06_12_032431) do
     t.string "address"
     t.string "note"
     t.integer "status", default: 0, null: false
-    t.bigint "users_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["users_id"], name: "index_orders_on_users_id"
-  end
-
-  create_table "person_post_news", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.string "image"
-    t.text "description"
-    t.bigint "users_id"
-    t.datetime "updated_at", null: false
-    t.index ["users_id"], name: "index_person_post_news_on_users_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -98,22 +87,20 @@ ActiveRecord::Schema.define(version: 2019_06_12_032431) do
     t.float "price_out"
     t.float "price_sale"
     t.integer "status", default: 0, null: false
-    t.bigint "small_categories_id"
+    t.bigint "small_category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["small_categories_id"], name: "index_products_on_small_categories_id"
+    t.index ["small_category_id"], name: "index_products_on_small_category_id"
   end
 
   create_table "ranks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "star_level"
-    t.bigint "users_id"
-    t.bigint "products_id"
-    t.bigint "person_post_news_id"
+    t.bigint "user_id"
+    t.bigint "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["person_post_news_id"], name: "index_ranks_on_person_post_news_id"
-    t.index ["products_id"], name: "index_ranks_on_products_id"
-    t.index ["users_id"], name: "index_ranks_on_users_id"
+    t.index ["product_id"], name: "index_ranks_on_product_id"
+    t.index ["user_id"], name: "index_ranks_on_user_id"
   end
 
   create_table "slides", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -126,10 +113,10 @@ ActiveRecord::Schema.define(version: 2019_06_12_032431) do
   create_table "small_categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "small_category_name"
     t.text "description"
-    t.bigint "categories_id"
+    t.bigint "category_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["categories_id"], name: "index_small_categories_on_categories_id"
+    t.index ["category_id"], name: "index_small_categories_on_category_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -145,20 +132,17 @@ ActiveRecord::Schema.define(version: 2019_06_12_032431) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
-  add_foreign_key "comments", "person_post_news"
-  add_foreign_key "comments", "products", column: "products_id"
-  add_foreign_key "comments", "users", column: "users_id"
-  add_foreign_key "contacts", "users", column: "users_id"
-  add_foreign_key "histories", "users", column: "users_id"
-  add_foreign_key "image_products", "products", column: "products_id"
-  add_foreign_key "order_details", "histories", column: "histories_id"
-  add_foreign_key "order_details", "orders", column: "orders_id"
-  add_foreign_key "order_details", "products", column: "products_id"
-  add_foreign_key "orders", "users", column: "users_id"
-  add_foreign_key "person_post_news", "users", column: "users_id"
-  add_foreign_key "products", "small_categories", column: "small_categories_id"
-  add_foreign_key "ranks", "person_post_news"
-  add_foreign_key "ranks", "products", column: "products_id"
-  add_foreign_key "ranks", "users", column: "users_id"
-  add_foreign_key "small_categories", "categories", column: "categories_id"
+  add_foreign_key "comments", "products"
+  add_foreign_key "comments", "users"
+  add_foreign_key "contacts", "users"
+  add_foreign_key "histories", "users"
+  add_foreign_key "image_products", "products"
+  add_foreign_key "order_details", "histories"
+  add_foreign_key "order_details", "orders"
+  add_foreign_key "order_details", "products"
+  add_foreign_key "orders", "users"
+  add_foreign_key "products", "small_categories"
+  add_foreign_key "ranks", "products"
+  add_foreign_key "ranks", "users"
+  add_foreign_key "small_categories", "categories"
 end
