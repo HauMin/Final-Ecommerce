635a551c3bc2f2dec3f0b9d25e4f861807d84b97
HauMin <haumin1201@gmail.com>
2019-06-16 14:35:31 +0700

    Admin Image_Products

--- config/routes.rb
+++ config/routes.rb
@@ -22,4 +22,5 @@ Rails.application.routes.draw do
   resources :categories
   resources :small_categories
   resources :products
+  resources :image_products
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -32,7 +32,15 @@ categories = Category.create(name: "quan", description: "quan mat")
 small_categories = SmallCategory.create(small_category_name: "Ao lot", description: "de mat", category_id: 1)
 small_categories = SmallCategory.create(small_category_name: "quan lot", description: "de mat", category_id: 2)
 
+<<<<<<< e91fa5938ad6c24f281149ba8558a669f2abc4f6
 products = Product.create(name: "sony", description_sort: "a", description_long: "b", image: "links",
 	color: "c", size: "3x4", price_in: 15, price_out: 20, price_sale: 3, status: 0, small_category_id: 1)
 products = Product.create(name: "apple", description_sort: "g", description_long: "h", image: "links",
 	color: "j", size: "3x4", price_in: 20, price_out: 25, price_sale: 0, status: 1, small_category_id: 2)
+=======
+#products = Product.create(name: "sony", description_sort: "a", description_long: "b", image: "",
+#	color: "c", size: "3x4", price_in: 15, price_out: 20, price_sale: 3, status: 0, small_category_id: 1)
+#products = Product.create(name: "apple", description_sort: "g", description_long: "h", image: "",
+#	color: "j", size: "3x4", price_in: 20, price_out: 25, price_sale: 0, status: 1, small_category_id: 2)
+#image_products = ImageProduct.create(image: "", product_id: 1)
+>>>>>>> Admin Image_Products
