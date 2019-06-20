12a7d088cb68714c5b4d003afa3bae2fab829eed
HauMin <haumin1201@gmail.com>
2019-06-20 11:37:28 +0700

    final ecommerce

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,27 +25,27 @@
 
 # contacts = Contact.create(users_id: 1,message: "sydgasda")
 
-categories = Category.create(name: "TOPS",description: "tops")
-categories = Category.create(name: "BOTTOMS",description: "bottoms")
-categories = Category.create(name: "ACCESSORIES",description: "accessories")
-small_categories = SmallCategory.create(small_category_name: "Blouses Shirts",
-  description: "Blouses Shirts", category_id: 1)
-small_categories = SmallCategory.create(small_category_name: "Dresses",
-  description: "Dresses", category_id: 1)
-small_categories = SmallCategory.create(small_category_name: "Tops & T shirts",
-  description: "Tops & T shirts", category_id: 1)
-small_categories = SmallCategory.create(small_category_name: "Trousers",
-  description: "Trousers", category_id: 2)
-small_categories = SmallCategory.create(small_category_name: "Jeans",
-  description: "Jeans", category_id: 2)
-small_categories = SmallCategory.create(small_category_name: "Leggings",
-  description: "Leggings", category_id: 2)
-small_categories = SmallCategory.create(small_category_name: "Jewellery",
-  description: "Jewellery", category_id: 3)
-small_categories = SmallCategory.create(small_category_name: "Hats",
-  description: "Hats", category_id: 3)
-small_categories = SmallCategory.create(small_category_name: "Scarves & snoods",
-  description: "Scarves & snoods", category_id: 3)
+# categories = Category.create(name: "TOPS",description: "tops")
+# categories = Category.create(name: "BOTTOMS",description: "bottoms")
+# categories = Category.create(name: "ACCESSORIES",description: "accessories")
+# small_categories = SmallCategory.create(small_category_name: "Blouses Shirts",
+#   description: "Blouses Shirts", category_id: 1)
+# small_categories = SmallCategory.create(small_category_name: "Dresses",
+#   description: "Dresses", category_id: 1)
+# small_categories = SmallCategory.create(small_category_name: "Tops & T shirts",
+#   description: "Tops & T shirts", category_id: 1)
+# small_categories = SmallCategory.create(small_category_name: "Trousers",
+#   description: "Trousers", category_id: 2)
+# small_categories = SmallCategory.create(small_category_name: "Jeans",
+#   description: "Jeans", category_id: 2)
+# small_categories = SmallCategory.create(small_category_name: "Leggings",
+#   description: "Leggings", category_id: 2)
+# small_categories = SmallCategory.create(small_category_name: "Jewellery",
+#   description: "Jewellery", category_id: 3)
+# small_categories = SmallCategory.create(small_category_name: "Hats",
+#   description: "Hats", category_id: 3)
+# small_categories = SmallCategory.create(small_category_name: "Scarves & snoods",
+#   description: "Scarves & snoods", category_id: 3)
 
 products = Product.create(name: "BODY SHAPING LEGGINGS",
   description_sort: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit",
@@ -57,130 +57,130 @@ products = Product.create(name: "BODY SHAPING LEGGINGS",
   price_out: 400000,
   price_sale: 50000,
   small_category_id: 4 )
-# products = Product.create(name: "CHOKER PACK",
-#   description_sort: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit",
-#   description_long: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.",
-#   image: "Web/CHOKER_PACK_large.jpg",
-#   color: "blue",
-#   size: "L",
-#   price_in: 100000,
-#   price_out: 200000,
-#   price_sale: 50000,
-#   small_category_id: 5 )
-# products = Product.create(name: "CONTRAST FRILLED DRESS",
-#   description_sort: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit",
-#   description_long: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.",
-#   image: "Web/CONTRAST_FRILLED_DRESS_large.jpg",
-#   color: "blue",
-#   size: "L",
-#   price_in: 450000,
-#   price_out: 600000,
-#   price_sale: 50000,
-#   small_category_id: 3 )
-# products = Product.create(name: "FABRIC TEXT BAG DETAILS",
-#   description_sort: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit",
-#   description_long: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.",
-#   image: "Web/FABRIC_TEXT_BAG_DETAILS_large.jpg",
-#   color: "blue",
-#   size: "L",
-#   price_in: 200000,
-#   price_out: 300000,
-#   price_sale: 50000,
-#   small_category_id: 8 )
-# products = Product.create(name: "FUR HIDE CLUTCHg",
-#   description_sort: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit",
-#   description_long: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.",
-#   image: "Web/FUR_HIDE_CLUTCH_large.jpg",
-#   color: "blue",
-#   size: "L",
-#   price_in: 239000,
-#   price_out: 339000,
-#   price_sale: 49000,
-#   small_category_id: 8 )
-# products = Product.create(name: "JACQUARD LEGGINGS",
-#   description_sort: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit",
-#   description_long: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.",
-#   image: "Web/JACQUARD_LEGGINGS_3_62417492-3e6a-40e2-b0dc-9a438e4e987c_large.jpg",
-#   color: "blue",
-#   size: "L",
-#   price_in: 499000,
-#   price_out: 699000,
-#   price_sale: 50000,
-#   small_category_id: 2 )
-# products = Product.create(name: "JACQUARD LEGGINGS large",
-#   description_sort: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit",
-#   description_long: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.",
-#   image: "Web/JACQUARD_LEGGINGS_large.jpg",
-#   color: "blue",
-#   size: "L",
-#   price_in: 300000,
-#   price_out: 400000,
-#   price_sale: 50000,
-#   small_category_id: 2 )
-# products = Product.create(name: "LEATHER FUR CROSSBODY BAG",
-#   description_sort: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit",
-#   description_long: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.",
-#   image: "Web/LEATHER_FUR_CROSSBODY_BAG_4_large.jpg",
-#   color: "blue",
-#   size: "L",
-#   price_in: 300000,
-#   price_out: 450000,
-#   price_sale: 50000,
-#   small_category_id: 6 )
-# products = Product.create(name: "LIMITED EDITION BOMBER JACKET",
-#   description_sort: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit",
-#   description_long: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.",
-#   image: "Web/LIMITED_EDITION_BOMBER_JACKET_3_ea65a664-7e5b-417b-8b5f-a4ff5462cc6f_large.jpg",
-#   color: "blue",
-#   size: "L",
-#   price_in: 499000,
-#   price_out: 599000,
-#   price_sale: 50000,
-#   small_category_id: 7 )
-# products = Product.create(name: "ORIENTAL PRINT BODYSUIT",
-#   description_sort: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit",
-#   description_long: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.",
-#   image: "Web/ORIENTAL_PRINT_BODYSUIT_1_854af841-3210-4688-b541-5fc70c49d0f2_large.jpg",
-#   color: "blue",
-#   size: "L",
-#   price_in: 599000,
-#   price_out: 799000,
-#   price_sale: 50000,
-#   small_category_id: 6 )
-# products = Product.create(name: "QUILTED WAISTCOAT",
-#   description_sort: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit",
-#   description_long: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.",
-#   image: "Web/QUILTED_WAISTCOAT_large.jpg",
-#   color: "blue",
-#   size: "L",
-#   price_in: 349000,
-#   price_out: 500000,
-#   price_sale: 50000,
-#   small_category_id: 9 )
-# products = Product.create(name: "PLEATED TROUSERS",
-#   description_sort: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit",
-#   description_long: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.",
-#   image: "Web/PLEATED_TROUSERS_large.jpg",
-#   color: "blue",
-#   size: "L",
-#   price_in: 449000,
-#   price_out: 649000,
-#   price_sale: 50000,
-#   small_category_id: 7 )
-# products = Product.create(name: "PINSTRIPE BOX PLEAT SKIRT",
-#   description_sort: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit",
-#   description_long: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.",
-#   image: "Web/PINSTRIPE_BOX_PLEAT_SKIRT._1_large.jpg",
-#   color: "blue",
-#   size: "L",
-#   price_in: 200000,
-#   price_out: 300000,
-#   price_sale: 50000,
-#   small_category_id: 4 )
+products = Product.create(name: "CHOKER PACK",
+  description_sort: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit",
+  description_long: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.",
+  image: "Web/CHOKER_PACK_large.jpg",
+  color: "blue",
+  size: "L",
+  price_in: 100000,
+  price_out: 200000,
+  price_sale: 50000,
+  small_category_id: 5 )
+products = Product.create(name: "CONTRAST FRILLED DRESS",
+  description_sort: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit",
+  description_long: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.",
+  image: "Web/CONTRAST_FRILLED_DRESS_large.jpg",
+  color: "blue",
+  size: "L",
+  price_in: 450000,
+  price_out: 600000,
+  price_sale: 50000,
+  small_category_id: 3 )
+products = Product.create(name: "FABRIC TEXT BAG DETAILS",
+  description_sort: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit",
+  description_long: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.",
+  image: "Web/FABRIC_TEXT_BAG_DETAILS_large.jpg",
+  color: "blue",
+  size: "L",
+  price_in: 200000,
+  price_out: 300000,
+  price_sale: 50000,
+  small_category_id: 8 )
+products = Product.create(name: "FUR HIDE CLUTCHg",
+  description_sort: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit",
+  description_long: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.",
+  image: "Web/FUR_HIDE_CLUTCH_large.jpg",
+  color: "blue",
+  size: "L",
+  price_in: 239000,
+  price_out: 339000,
+  price_sale: 49000,
+  small_category_id: 8 )
+products = Product.create(name: "JACQUARD LEGGINGS",
+  description_sort: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit",
+  description_long: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.",
+  image: "Web/JACQUARD_LEGGINGS_3_62417492-3e6a-40e2-b0dc-9a438e4e987c_large.jpg",
+  color: "blue",
+  size: "L",
+  price_in: 499000,
+  price_out: 699000,
+  price_sale: 50000,
+  small_category_id: 2 )
+products = Product.create(name: "JACQUARD LEGGINGS large",
+  description_sort: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit",
+  description_long: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.",
+  image: "Web/JACQUARD_LEGGINGS_large.jpg",
+  color: "blue",
+  size: "L",
+  price_in: 300000,
+  price_out: 400000,
+  price_sale: 50000,
+  small_category_id: 2 )
+products = Product.create(name: "LEATHER FUR CROSSBODY BAG",
+  description_sort: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit",
+  description_long: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.",
+  image: "Web/LEATHER_FUR_CROSSBODY_BAG_4_large.jpg",
+  color: "blue",
+  size: "L",
+  price_in: 300000,
+  price_out: 450000,
+  price_sale: 50000,
+  small_category_id: 6 )
+products = Product.create(name: "LIMITED EDITION BOMBER JACKET",
+  description_sort: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit",
+  description_long: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.",
+  image: "Web/LIMITED_EDITION_BOMBER_JACKET_3_ea65a664-7e5b-417b-8b5f-a4ff5462cc6f_large.jpg",
+  color: "blue",
+  size: "L",
+  price_in: 499000,
+  price_out: 599000,
+  price_sale: 50000,
+  small_category_id: 7 )
+products = Product.create(name: "ORIENTAL PRINT BODYSUIT",
+  description_sort: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit",
+  description_long: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.",
+  image: "Web/ORIENTAL_PRINT_BODYSUIT_1_854af841-3210-4688-b541-5fc70c49d0f2_large.jpg",
+  color: "blue",
+  size: "L",
+  price_in: 599000,
+  price_out: 799000,
+  price_sale: 50000,
+  small_category_id: 6 )
+products = Product.create(name: "QUILTED WAISTCOAT",
+  description_sort: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit",
+  description_long: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.",
+  image: "Web/QUILTED_WAISTCOAT_large.jpg",
+  color: "blue",
+  size: "L",
+  price_in: 349000,
+  price_out: 500000,
+  price_sale: 50000,
+  small_category_id: 9 )
+products = Product.create(name: "PLEATED TROUSERS",
+  description_sort: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit",
+  description_long: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.",
+  image: "Web/PLEATED_TROUSERS_large.jpg",
+  color: "blue",
+  size: "L",
+  price_in: 449000,
+  price_out: 649000,
+  price_sale: 50000,
+  small_category_id: 7 )
+products = Product.create(name: "PINSTRIPE BOX PLEAT SKIRT",
+  description_sort: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit",
+  description_long: "Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex.",
+  image: "Web/PINSTRIPE_BOX_PLEAT_SKIRT._1_large.jpg",
+  color: "blue",
+  size: "L",
+  price_in: 200000,
+  price_out: 300000,
+  price_sale: 50000,
+  small_category_id: 4 )
 
-# ranks = Rank.create(star_level: 5,
-#   user_id: 1,
-#   product_id: 1 )
+ranks = Rank.create(star_level: 5,
+  user_id: 1,
+  product_id: 1 )
 
 
 
@@ -194,8 +194,8 @@ products = Product.create(name: "BODY SHAPING LEGGINGS",
 # 	color: "c", size: "3x4", price_in: 15, price_out: 20, price_sale: 3, status: 0, small_category_id: 1)
 # products = Product.create(name: "apple", description_sort: "g", description_long: "h", image: "links",
 # 	color: "j", size: "3x4", price_in: 20, price_out: 25, price_sale: 0, status: 1, small_category_id: 2)
-#products = Product.create(name: "sony", description_sort: "a", description_long: "b", image: "",
-#	color: "c", size: "3x4", price_in: 15, price_out: 20, price_sale: 3, status: 0, small_category_id: 1)
+# products = Product.create(name: "sony", description_sort: "a", description_long: "b", image: "",
+# 	color: "c", size: "3x4", price_in: 15, price_out: 20, price_sale: 3, status: 0, small_category_id: 1)
 #products = Product.create(name: "apple", description_sort: "g", description_long: "h", image: "",
 #	color: "j", size: "3x4", price_in: 20, price_out: 25, price_sale: 0, status: 1, small_category_id: 2)
 #image_products = ImageProduct.create(image: "", product_id: 1)
@@ -203,11 +203,11 @@ products = Product.create(name: "BODY SHAPING LEGGINGS",
 # small_categories = SmallCategory.create(small_category_name: "Blouses Shirts",
 #   description: "Blouses Shirts")
 
-users = User.create(name: "Ngo Minh Hau", phone: "0123456789", email: "haumin1201@gmail.com", 
-	address: "Da Nang", password: "123123", role: 1)
+# users = User.create(name: "Ngo Minh Hau", phone: "0123456789", email: "haumin1201@gmail.com", 
+# 	address: "Da Nang", password: "123123", role: 1)
 
-users = User.create(name: "Pham Den", phone: "0123456789", email: "denpham1201@gmail.com", 
-	address: "Da Nang", password: "123123", role: 0)
+# users = User.create(name: "Pham Den", phone: "0123456789", email: "denpham1201@gmail.com", 
+# 	address: "Da Nang", password: "123123", role: 0)
 
 # orders = Order.create(date_order: "", address: "abc", note:"abc", status: 0, user_id: 1) 
 # orders = Order.create(date_order: "", address: "123", note:"123", status: 1, user_id: 2) 
